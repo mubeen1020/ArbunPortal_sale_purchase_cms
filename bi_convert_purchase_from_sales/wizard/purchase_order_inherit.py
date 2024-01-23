89d6a9655d08f64c17494b26b9da5cdc84fb2525
mubeen1020 <m.mubeen1020@gmail.com>
2024-01-23 09:11:40 +0500

    working

--- bi_convert_purchase_from_sales/wizard/purchase_order_inherit.py
+++ bi_convert_purchase_from_sales/wizard/purchase_order_inherit.py
@@ -6,3 +6,4 @@ class PurchaseOrder(models.Model):
     booking_datetime = fields.Datetime('Date and Time of Booking')
     customer_name = fields.Char(string='Customer Name')
     mode_of_payment = fields.Char(string='Mode of Payment')
+  
--- custom_sales_menu/models/models.py
+++ custom_sales_menu/models/models.py
@@ -1,6 +1,11 @@
 from odoo import models, fields, api
 from datetime import timedelta
 
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
 class SaleOrderInherit(models.Model):
     _inherit = 'sale.order'
 
@@ -16,3 +21,26 @@ class SaleOrderInherit(models.Model):
                 record.adjusted_booking_time = adjusted_time.strftime('%H:%M:%S')
             else:
                 record.adjusted_booking_time = False
+
+    vendor_names1 = fields.Char(
+        string='Vendors', compute='_compute_vendor_names'
+    )
+
+    @api.depends('partner_id.name')
+    def _compute_vendor_names(self):
+        for record in self:
+                vendors = set()
+                related_pos = self.env['purchase.order'].search([('origin','=',record.name)])
+                for po in related_pos:
+                    vendors.add(po.partner_id.name)
+                record.vendor_names1 = ", ".join(vendors)    
+
+            
+    display_name = fields.Char(compute='_compute_display_name')
+
+    @api.depends('partner_id.name', 'vendor_names1')
+    def _compute_display_name(self):
+        for record in self:
+            customer_name = record.partner_id.name if record.partner_id else ''
+            vendors = record.vendor_names1 if record.vendor_names1 else 'No Vendors'
+            record.display_name = f"{customer_name} ({vendors})"
